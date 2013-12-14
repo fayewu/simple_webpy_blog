8bf1527cbfb128ddf1b33acf2f557f65016c8acb
fayewu <fayewu37@gmail.com>
2013-12-14 22:53:15 +0800

    delete

--- config/setting.py
+++ config/setting.py
@@ -8,11 +8,6 @@ from url import urls
 web.config.debug = False
 app = web.application(urls, globals()).wsgifunc()
 
-db = web.database(dbn = "mysql", user = "faye", pw = "wufeishizhu.", 
-        db = "web_test");
-
-store = web.session.DBStore(db, "sessions")
-session = web.session.Session(app, store, initializer={'login': 0})
 #web.config_session = session
 
 #render_login = web.template.render("templates/", globals = {'context':session})
