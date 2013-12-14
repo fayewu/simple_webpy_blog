672b100a9efa4de433bc6f55e3b0d7b9a12ac6b0
fayewu <fayewu37@gmail.com>
2013-12-14 22:28:32 +0800

    wsgi

--- config/setting.py
+++ config/setting.py
@@ -6,7 +6,7 @@ import web
 from url import urls
 
 web.config.debug = False
-app = web.application(urls, globals())
+app = web.application(urls, globals()).wsgifunc()
 
 db = web.database(dbn = "mysql", user = "faye", pw = "wufeishizhu.", 
         db = "web_test");
