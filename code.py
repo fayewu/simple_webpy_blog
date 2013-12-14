6ebf9c38f75eca3acb2dc23533ed877452e26c22
fayewu <fayewu37@gmail.com>
2013-12-14 23:17:58 +0800

    apache

--- code.py
+++ code.py
@@ -9,7 +9,6 @@ os.chdir(abspath)
 import web
 
 from controllers.index import Index
-from config.setting import app
 from config.url import urls
 from controllers.init import init_list
 from controllers.init import blog_posts
@@ -17,4 +16,4 @@ from controllers.init import blog_posts
 
 if __name__ == "__main__":
     init_list()
-    app.wsgifunc()
+    app = web.application(urls, globals()).wsgifunc()
