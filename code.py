44652bc290627cd8d76e765dff464db079818e71
fayewu <fayewu37@gmail.com>
2013-12-14 23:13:21 +0800

    apache

--- code.py
+++ code.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import sys, os
+abspath = os.path.dirname(__file__)
+sys.path.append(abspath)
+os.chdir(abspath)
+
+import web
 
 from controllers.index import Index
 from config.setting import app
@@ -8,12 +14,6 @@ from config.url import urls
 from controllers.init import init_list
 from controllers.init import blog_posts
 
-import sys, os
-abspath = os.path.dirname(__file__)
-sys.path.append(abspath)
-os.chdir(abspath)
-
-import web
 
 if __name__ == "__main__":
     init_list()
