237c8547c33855ebb0f27472568e45d3afff793b
fayewu <fayewu37@gmail.com>
2013-12-16 21:19:32 +0800

    change blog time

--- controllers/init.py
+++ controllers/init.py
@@ -2,6 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import os
+import time
 import codecs
 import urllib
 import markdown
@@ -13,6 +14,7 @@ class Articles(object):
     def __init__(self, filename):
         self.name = filename.replace(".md", "");
         self.time, self.title = self.name.split("_") 
+        self.ctime = time.ctime(os.path.getctime(file_path + filename))
         self.url = "/blog/" + self.time.replace("-", "/") + "/" + self.title
         
         input_file = codecs.open(file_path + filename, mode = "r", 
@@ -20,8 +22,9 @@ class Articles(object):
         self.html = markdown.markdown(input_file.read())
 
 def init_list():
-    file_list = os.listdir("contents/")
+    file_list = os.listdir(file_path)
 
     for each in file_list:
             blog_posts.append(Articles(each)) 
 
+    blog_posts.sort(cmp = lambda x,y:cmp(x.ctime, y.ctime), reverse = True)
